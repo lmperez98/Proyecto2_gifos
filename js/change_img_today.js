1a3723bc114598f0d40a198e0e17bdf22b90a78e
dcabulo <gcabulo@gmail.com>
2020-04-04 21:59:09 -0500

    js_change

--- js/change_img_today.js
+++ js/change_img_today.js
@@ -4,7 +4,7 @@ async function change_img_today() {
         const response = await fetch("https://api.giphy.com/v1/gifs/trending?api_key=uQN81XZ1Dt44mlcLeDhKbjAG4UMv6szw&limit=15&rating=PG-13")
         const data = await response.json()
         console.log(data)
-        change_img[0].setAttribute("src",data.data[0].images.fixed_width.url)
+        change_img[0].setAttribute("src",data.data[0].images.fixed_height.url)
     }
     catch(fatal_error){
         console.log("fetch fail?ed",fatal_error)
