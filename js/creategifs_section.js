8f57d5606ce55417e065b79dcebbe6ac32436b85
diego cabulo <gcabulo@gmai.com>
2020-05-06 09:30:51 -0500

    Loadbar
    loadbar dinamico y play button

--- js/creategifs_section.js
+++ js/creategifs_section.js
@@ -1,13 +1,14 @@
-const createSpans = (limit, term) => {
+const createSpans = (limit, term, styleclass) => {
     let container = document.getElementsByClassName(term)[0]
     for (let i = 0; i < limit; i++) {
         const spanbox = document.createElement("span");
+        spanbox.setAttribute("class", styleclass)
         container.appendChild(spanbox)
     }
 }
 
-createSpans(16, "loadbarbox")
-createSpans(21, "loadbar_upload")
+createSpans(16, "loadbarbox", "loadbar_video")
+createSpans(21, "loadbar_upload", "upload_bar")
 
 
 const MisGifsButton = document.getElementsByClassName("mis_gifs")[0]
@@ -18,7 +19,10 @@ const stopRecord = document.getElementsByClassName("button_ready")[0]
 const uploadGif = document.getElementsByClassName("upload_gif")[0]
 const againButton = document.getElementsByClassName("repeat_gif")[0]
 const videoPreview = document.getElementsByClassName("video_elemet")[0]
+const playGifButton = document.getElementsByClassName("playgif")[0]
 let timerBox = document.getElementsByClassName("timerbox")[0]
+const loadbarspan = document.getElementsByClassName("loadbar_video")
+const uploadBarBox = document.getElementsByClassName("upload_bar")
 let watch = new stopwatch(timerBox)
 
 const setRecorder = async () => {
@@ -37,6 +41,32 @@ const setRecorder = async () => {
         showElements("window_error_upload")
         document.getElementsByClassName("error_msg")[0].textContent = "Fatal error..." + err
     }
+}
+
+const loadingBar = (index, time, color) => {
+    for (let i = 1; i < index + 1; i++) {
+        setTimeout(() => {
+            loadbarspan[i - 1].style.backgroundColor = color
+        }, time * i)
+    }
+}
+
+let intervalTImer
+const uploadBar = (color1, color2) => {
+    let counter1 = 0
+    let counter2 = 0
+    intervalTImer = setInterval(() => {
+        if (counter1 < uploadBarBox.length && counter1 != 21) {
+            uploadBarBox[counter1].style.backgroundColor = color1
+            counter1++
+        } else if (counter2 < uploadBarBox.length) {
+            uploadBarBox[counter2].style.backgroundColor = color2
+            counter2++
+        } else {
+            counter1 = 0
+            counter2 = 0
+        }
+    }, 200);
 }
 
 MisGifsButton.onclick = () => {
@@ -65,6 +95,10 @@ stopRecord.onclick = () => {
     watch.stop()
 }
 
+playGifButton.onclick = () => {
+    loadingBar(16, 0, "#999999")
+    loadingBar(16, watch.timeFinale / 16, "#F7C9F3")
+}
 
 againButton.onclick = () => {
     hideElements("loadbar", "repeat_uploadgif")
@@ -75,6 +109,7 @@ againButton.onclick = () => {
 uploadGif.onclick = () => {
     hideElements("window_scene2")
     showElements("window_scene5")
+    uploadBar("#F7C9F3", "#999999")
 }
 
 const hideElements = (...term) => {
